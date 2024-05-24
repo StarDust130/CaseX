7776fb522c1f7fc9b9811af78b3314ca0ae5b637
ChandraShekhar <csyadav0513@gmail.com>
2024-05-24 13:14:58 +0530

    Add DesignConfigurator component for design configuration

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,11 @@
+
+interface DesignConfigProps {
+    imageUrl: string;
+    width: number;
+    height: number;
+    }
+
+const DesignConfigurator = ({ imageUrl, width, height }: DesignConfigProps) => {
+  return <div>DesignConfigurator</div>;
+};
+export default DesignConfigurator;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,8 +1,34 @@
+import DesignConfigurator from "./DesignConfigurator";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 
-
-const page = async() => {
-  return (
-    <div>page</div>
-  )
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
-export default page
+
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  //! If Id is not there  return a 404 😆
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  //! If configuration is not found return a 404 😆
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator imageUrl={imageUrl} width={width} height={height} />
+  );
+};
+export default page;
