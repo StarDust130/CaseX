473dfa9d66d335ae5f50588f19861fe7905d186f
ChandraShekhar <csyadav0513@gmail.com>
2024-05-24 21:44:35 +0530

    feat: Add @headlessui/react and @radix-ui/react-label dependencies

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,26 @@
+// bg-blue-950 border-blue-950
+// bg-rose-500 border-rose-500
+// bg-zinc-900 border-zinc-900
+// bg-yellow-950 border-yellow-950
+// bg-green-950 border-green-950
+// bg-red-950 border-red-950
+// bg-purple-950 border-purple-950
+// bg-orange-950 border-orange-950
+// bg-gray-500 border-gray-500
+// bg-pink-950 border-pink-950
+// bg-brown-950 border-brown-950
+
+
+
+
+
+export const COLORS = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  { label: "Transparent", value: "transparent", tw: "transparent" },
+  { label: "Blue", value: "blue", tw: "blue-950" },
+  { label: "Rose", value: "rose", tw: "rose-950" },
+  { label: "Yellow", value: "yellow", tw: "yellow-950" },
+  { label: "Green", value: "green", tw: "green-950" },
+  { label: "Gray", value: "gray", tw: "gray-500" },
+  
+] as const;
